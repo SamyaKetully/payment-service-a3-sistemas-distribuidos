1b3a0cf20bac4696116b9457896c6cbdf689676c
SamyaKetully <samyaketully62@gmail.com>
2026-05-03 15:04:31 -0300

    First commit

---
+++ src/main/java/com/example/testpaymentservice/TestPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpaymentservice/TestPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
