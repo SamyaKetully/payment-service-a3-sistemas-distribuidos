ce80f96345d7b31f5336a9b356568dedba82f907
SamyaKetully <10723113963@ulife.com.br>
2026-05-31 00:31:40 -0300

    added logic to Controller, pom dependencies, DTO, Service e properties

--- src/main/java/com/samyasotero/testpaymentservice/dto/PaymentRequestDTO.java
+++ src/main/java/com/samyasotero/testpaymentservice/dto/PaymentRequestDTO.java
@@ -1,4 +1,34 @@
 package com.samyasotero.testpaymentservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import org.jetbrains.annotations.NotNull;
+import java.math.BigDecimal;
+
 public class PaymentRequestDTO {
+
+    private Long orderId;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @NotBlank
+    private String paymentMethod;
+
+    public PaymentRequestDTO() {
+    }
+
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+
+    public String getPaymentMethod() { return paymentMethod; }
+    public void setPaymentMethod(String paymentMethod) { this.paymentMethod = paymentMethod; }
 }
